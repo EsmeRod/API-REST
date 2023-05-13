95f42af9b7e160961d996e2bd5a3ee3ee8202ab5
EsmeRod <89407510+EsmeRod@users.noreply.github.com>
2023-05-12 21:19:58 -0600

    App2
    Activitys y XML

---
+++ APIREST/app/src/main/java/sv/edu/udb/api_rest/Activity_ActualizarAlumno.kt
@@ -0,0 +1,11 @@
+package sv.edu.udb.api_rest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_ActualizarAlumno : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actualizar_alumno)
+    }
+}
---
+++ APIREST/app/src/main/java/sv/edu/udb/api_rest/Activity_Profesor_Main.kt
@@ -0,0 +1,11 @@
+package sv.edu.udb.api_rest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Profesor_Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profesor_main)
+    }
+}
